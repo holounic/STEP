7eb43c76db6fa9e75651ba2754295382ac7d5883
Masha <mia.ilminsckaya@yandex.ru>
2020-07-16 14:22:56 +0000

    fix js code style

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,7 +1,12 @@
-const facts = ['i love cats', 'i love programming', 'i love sundays', 'i love cakes']
+const facts = [
+    'i love cats', 
+    'i love programming', 
+    'i love sundays', 
+    'i love cakes'
+    ]
 
 function getRandomFact() {
-    const fact = facts[Math.floor(Math.random() * facts.length)];
-    const factContainer = document.getElementById("fact-container");
-    factContainer.innerText = fact;
+  const fact = facts[Math.floor(Math.random() * facts.length)];
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
 }
