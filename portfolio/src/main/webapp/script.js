3cb999f86705acd1773c6570c607b951e2a24a13
Masha <mia.ilminsckaya@yandex.ru>
2020-07-17 00:39:47 +0000

    replace tabs with two spaces

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,9 +1,9 @@
 const facts = [
-    'i love cats', 
-    'i love programming', 
-    'i love sundays', 
-    'i love cakes'
-    ]
+  'i love cats', 
+  'i love programming', 
+  'i love sundays', 
+  'i love cakes'
+]
 
 function getRandomFact() {
   const fact = facts[Math.floor(Math.random() * facts.length)];
