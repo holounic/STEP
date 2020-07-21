71e2357623edd695bc44075c760ecd903155aff3
Masha <mia.ilminsckaya@yandex.ru>
2020-07-21 12:15:36 +0000

    fix indentation

--- portfolio/src/main/java/com/google/sps/servlets/Comment.java
+++ portfolio/src/main/java/com/google/sps/servlets/Comment.java
@@ -1,9 +1,9 @@
 package com.google.sps.servlets;
 
 public class Comment {
-    private final String text;
+  private final String text;
 
-    public Comment(String text) {
-        this.text = text;
-    }
+  public Comment(String text) {
+    this.text = text;
+  }
 }
